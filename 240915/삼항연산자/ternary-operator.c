5d155cb90c2b9ffb567d65a7c50eaa1464073f60
최지원 <jw20424@hufs.ac.kr>
2024-09-15 11:44:05 +0900

    Title: 삼항연산자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240915/삼항연산자/ternary-operator.c
+++ 240915/삼항연산자/ternary-operator.c
@@ -4,7 +4,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     scanf("%d", &a);
-    int grade = (a = 100) ? 0 : 1;
+    int grade = (a == 100) ? 0 : 1;
     if (grade == 0)
         printf("pass");
     else
