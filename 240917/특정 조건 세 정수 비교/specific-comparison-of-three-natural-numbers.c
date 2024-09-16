c45aa4156771aac65c1335b7eca3eb4a6744166d
최지원 <jw20424@hufs.ac.kr>
2024-09-17 01:21:36 +0900

    Title: 특정 조건 세 정수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240917/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a <= b && a <= c && b <= c)
+        if (a == c)
+            printf("1 ");
+        else
+            printf("0 ");
+    else
+        printf("0 ");
+    if (a == b == c)
+        printf("1");
+    else 
+        printf("0");
+    return 0;
+}
