3033ac1677de13ca43d6a0b38fcbb45ef30182a0
최지원 <jw20424@hufs.ac.kr>
2024-09-19 15:31:47 +0900

    Title: n부터 1까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240919/n부터 1까지 숫자 출력/print-n-to-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, i = 1;
+
+    scanf("%d", &n);
+
+    while(i <= n){
+        printf("%d ", n);
+        n --;
+    }
+    return 0;
+}
