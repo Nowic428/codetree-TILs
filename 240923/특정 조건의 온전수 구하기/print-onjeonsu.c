fe6b0a66203d44854dc7254a5ecc77bc089be1f9
최지원 <jw20424@hufs.ac.kr>
2024-09-23 15:19:49 +0900

    Title: 특정 조건의 온전수 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240923/특정 조건의 온전수 구하기/print-onjeonsu.c
+++ 240923/특정 조건의 온전수 구하기/print-onjeonsu.c
@@ -8,7 +8,11 @@ int main() {
 
     for (int i = 1; i <= n; i++)
     {
-        if (i % 2 == 0 || (i % 3 == 0 && i % 9 != 0) || i % 10 == 5)
+        if(i % 2 == 0)
+            continue;
+        if(i % 10 == 0)
+            continue;
+        if(i % 3 == 0 && i % 9 != 0)
             continue;
         printf("%d ", i);
     }
