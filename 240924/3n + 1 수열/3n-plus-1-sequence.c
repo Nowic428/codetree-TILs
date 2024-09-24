5b3e1ee41643221ce511da866607fccd77b9a3fe
최지원 <jw20424@hufs.ac.kr>
2024-09-24 12:28:09 +0900

    Title: 3n + 1 수열, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240924/3n + 1 수열/3n-plus-1-sequence.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt = 0;
+
+    scanf("%d", &n);
+
+    while(1)
+    {
+        if(n % 2 == 0)
+            n /= 2;
+        else
+            n = (n*3+1);
+
+        cnt++;
+        
+        if(n == 1)
+        {
+            printf("%d", cnt);
+            break;
+        }
+    }
+    return 0;
+}
