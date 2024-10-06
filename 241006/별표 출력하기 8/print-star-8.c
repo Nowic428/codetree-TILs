f7d39d517f6452726b9683d04b98bea44c64cba5
최지원 <jw20424@hufs.ac.kr>
2024-10-06 20:13:34 +0900

    Title: 별표 출력하기 8, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241006/별표 출력하기 8/print-star-8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((i + 1) % 2 == 1)
+        {
+            printf("*");
+        }
+        else
+        {
+            for (int j = 0; j < i+1; j++)
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
